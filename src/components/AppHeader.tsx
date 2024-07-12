17096382c2835adeae5f7485d86cf29d7efc02fc
Ken Takahashi <takaken@redimpulz.com>
2024-07-12 23:56:52 +0900

    hotfix

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,4 +1,3 @@
-'use client';
 import Link from 'next/link';
 import { useContext } from 'react';
 import { signOut } from 'firebase/auth';
--- src/components/BookImage.tsx
+++ src/components/BookImage.tsx
@@ -1,12 +1,11 @@
-'use client';
 import Image from 'next/image';
 
-type Prop = {
+type Props = {
   src: string;
   className?: string;
 };
 
-export default function BookImage({ src, className }: Prop) {
+export default function BookImage({ src, className }: Props) {
   return (
     <div
       className={`mx-auto w-48 sm:w-60 min-h-48 grid place-items-center ${className}`}
